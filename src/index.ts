463ef54130ad6b23f0aa54dea557aa5c7a0e1cfa
Raz Gaon <razgaon@mit.edu>
2023-08-02 01:39:53 -0700

    Fixed code running in index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@ import { ArxivClient } from "./client/client";
 import { ArticleMetadata } from "./interface";
 
 export { ArxivClient, ArticleMetadata };
-
-const c = new ArxivClient();
-c.getArticle("https://arxiv.org/abs/2106.01401")
